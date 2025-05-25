7ae29fb5efbda442433729cb83c8a95c7cc09e05
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2025-05-25 13:58:00 -0300

    Add files via upload

---
+++ ProjetoAgrinhoAula-4/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Pergunta 1',
+    'Qual a importância do campo?',
+    'Garantir que os nossos alimentos sejam produzidos'
+)
+
+criaCartao(
+    'Pergunta 2',
+    'Alimentos do campo(exemplos)',
+    'Tomate, Alface, Rabanete, Cenoura e Batata'
+)
+
+criaCartao(
+    'Pergunta 3',
+    'Qual e o setor que administra a agricultura no Brasil?',
+    'Setor primario'
+)
+
+criaCartao(
+    'Pergunta 4',
+     'Qual o nome do conjunto de atividades econômicas que mexem com agricultura?',
+    'Agronégocio'
+)
