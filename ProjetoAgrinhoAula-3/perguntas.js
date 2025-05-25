736167989f9c855503cea9f757787966fd2fd3ec
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2025-05-25 13:27:38 -0300

    Add files via upload

---
+++ ProjetoAgrinhoAula-3/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Pergunta 1',
+    'Qual a importância do campo?',
+    'Garantir que os nossos alimentos sejam produzidos'
+)
+
+criaCartao(
+    'Pergunta 2',
+    'Alimentos do campo(exemplos)',
+    'Tomate, Alface, Rabanete, Cenoura e Batata'
+)
+
+criaCartao(
+    'Pergunta 3',
+    'Qual o maior jogo de Dragon ball na atualidade?',
+    'Dragon Ball Legends'
+)
+criaCartao(
+    'Pergunta 4',
+    'Qual o segundo protagonista de dragon ball?',
+    'Gohan, Filho de Son Goku'
+)
