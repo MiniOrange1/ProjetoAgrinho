a917473c34c333a81ec2a2fb0cb4c8120ee0ff31
MiniOrange1 <lucas.cunha.souza@escola.pr.gov.br>
2025-05-28 18:19:00 -0300

    Add files via upload

--- ProjetoAgrinhoAula-4/perguntas.js
+++ ProjetoAgrinhoAula-4/perguntas.js
@@ -21,3 +21,31 @@ criaCartao(
      'Qual o nome do conjunto de atividades econômicas que mexem com agricultura?',
     'Agronégocio'
 )
+criaCartao(
+    'Pergunta 5',
+    'Quantos produtores rurais o brasil possui?',
+    'Cerca de 5 milhões'
+)
+
+criaCartao(
+    'Pergunta 6',
+    'O Brasil possui mais vacas ou mais pessoas?',
+    'Vacas lideram com 250 milhões, enquanto a população do Brasil e de 214 milhões'
+)
+
+criaCartao(
+    'Pergunta 7',
+    'Qual e a produção em que o Brasil lidera?',
+    'Laranja'
+)
+
+criaCartao(
+    'Pergunta 8',
+     'Qual o prduto que o Brasil mais consome(fora da Ásia)?',
+    'Arroz'
+)
+criaCartao(
+    'Pergunta 9',
+     'Qual a especialidade em que o Brasil é lider?',
+    'Desenvolvimento tecnólogico no campo'
+)
